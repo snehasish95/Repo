a66b71750544e60a0f667c1ac2ee39a2f0b58bfb
smzidane <mohapatrasnehasish95@gmail.com>
2024-05-11 23:22:47 +0530

    added for linked lists

---
+++ main.cpp
@@ -0,0 +1,59 @@
+//add two numbers
+#include <iostream>
+using namespace std;
+
+class Node{
+  public:
+    int data;
+    Node* next;
+    Node(int _data) : data(_data), next(NULL) {}
+};
+int add(int a, int b)
+{
+	return (a + b);
+}
+int findLL(Node* head)
+{
+  Node* current = head;
+  int length = 0;
+  while(current != NULL)
+  {
+    length++;
+    current = current -> next;
+  }
+  return length;
+}
+int main()
+{
+  // add two numbers
+  int a = 5;
+  int b = 10;
+  int result = add(a,b);
+  std::cout << result << std::endl;
+  //
+
+  // Length of linked list
+  Node* n1 = new Node(5);
+  Node* n2 = new Node(11);
+  Node* n3 = new Node(2);
+  Node* n4 = new Node(9);
+  Node* n5 = new Node(3);
+  n1 -> next = n2;
+  n2 -> next = n3;
+  n3 -> next = n4;
+  n4 -> next = n5;
+
+  int length = findLL(n1);
+  std::cout << "Length of linked list is : " << length << std::endl;
+
+  delete n1;
+  delete n2;
+  delete n3;
+  delete n4;
+  delete n5;
+
+  //
+
+
+  return 0;
+}
